9534b9804dc9fc11ccdc55e73b879cdffa90995a
Darshan Hire <38034283+dhire05@users.noreply.github.com>
2018-05-17 15:35:18 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# cassandrainsertrecord
+---
+title: CassandraDB Insert Activity
+---
+
+# CassandraDB Insert Activity
+This activity allows you to Insert a record to a particular table from the CassandraDB server
+
+## Installation
+### Flogo CLI
+```bash
+flogo install github.com/dhire05/CassandraInsertRecord
+```
+
+## Schema
+Inputs and Outputs:
+
+```json
+{   
+  "inputs":[
+    {
+      "name": "ClusterIP",
+      "type": "string",
+	  "required": true      
+    },
+	{
+      "name": "Keyspace",
+      "type": "string",
+      "required": true
+    },
+	{
+      "name": "TableName",
+      "type": "string",
+      "required": true
+    },
+	 {
+      "name": "InsertQuery",
+      "type": "string",
+      "required": true
+    }
+  ],
+  "outputs": [
+    {
+      "name": "result",
+      "type": "any"
+    }
+  ]
+ }
+```
+## Settings
+| Setting        | Required | Description |
+|:---------------|:---------|:------------|
+| ClusterIP      | True     | The CassandraDB cluster instance |         
+| Keyspace       | True     | The name of the Keyspace
+| TableName      | True     | The name of table to delete record
+| InsertQuery    | True		| The insert query is to insert a record into the table|
+
+## Example
+The below example is to insert a record into CassandraDB
+
+```json
+{
+  "id": "CassandraDB_1",
+  "name": "CassandraDB connector",
+  "description": "Insert record into CassandraDB",
+  "activity": {
+    "ref": "github.com/dhire05/CassandraInsertRecord",
+    "input": {
+      "ClusterIP": "127.0.0.1",
+      "Keyspace": "sample",
+      "TableName": "employee",
+	  "InsertQuery": "INSERT INTO employee (empid , name, salary) VALUES (103, 'lmn', 4044.40)"          
+    }
+  }
+}
+```
---
+++ activity.go
@@ -0,0 +1,88 @@
+package cassandrainsertrecord
+
+import (
+	"fmt"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/gocql/gocql"
+)
+
+// THIS IS ADDED
+// log is the default package logger which we'll use to log
+var log = logger.GetLogger("activity-CassandraInsertRecord")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+	// Get the activity data from the context
+	clusterIP := context.GetInput("ClusterIP").(string)
+	keySpace := context.GetInput("Keyspace").(string)
+	tableName := context.GetInput("TableName").(string)
+	insertQuery := context.GetInput("InsertQuery").(string)
+
+	// Use the log object to log the greeting
+	log.Debugf("The Flogo engine says connect to [%s] with  [%s] with table [%s]", clusterIP, keySpace, tableName)
+	log.Debugf("Flogo is about to insert [%s]  on cluster [%s]", insertQuery, clusterIP)
+	
+	fmt.Println("The Flogo engine says connect to "+clusterIP+" with  "+keySpace+" with table "+tableName)
+	fmt.Println("Flogo is about to insert "+insertQuery+"  on cluster "+clusterIP)
+
+	// Provide the cassandra cluster instance here.
+	cluster := gocql.NewCluster(clusterIP)
+
+	// gocql requires the keyspace to be provided before the session is created.
+	// In future there might be provisions to do this later.
+	cluster.Keyspace = keySpace
+	
+	session, err := cluster.CreateSession()
+	log.Debugf("Session Created Sucessfully")
+	fmt.Println("Session Created Sucessfully")
+
+	if err != nil {
+		log.Debugf("Could not connect to cassandra cluster : " , err)
+		fmt.Println("Could not connect to cassandra cluster : " , err)
+	}
+	log.Debugf("Session : ", session)
+	log.Debugf("Cluster : ", clusterIP)
+	log.Debugf("Keyspace : ", keySpace)
+	log.Debugf("Session Timeout : ", cluster.Timeout)
+	log.Debugf("TableName : ", tableName)
+	log.Debugf("Insert Query : ", insertQuery)
+
+	log.Debugf("Next Step is Insert Query Execution")
+
+	//fmt.Println("Session : ", session)
+	//fmt.Println("Cluster : ", clusterIP)
+	//fmt.Println("Keyspace : ", keySpace)
+	//fmt.Println("Session Timeout : ", cluster.Timeout)
+	//fmt.Println("TableName : ", tableName)
+	//fmt.Println("Insert Query : ", insertQuery)
+
+	fmt.Println("Next Step is Insert Query Execution")
+	
+	if err := session.Query(insertQuery).Exec(); err != nil {
+		log.Debugf("Error In insert Query : " , err)
+		fmt.Println("Error In insert Query : " , err)
+	}
+	
+
+	// Set the result as part of the context
+	context.SetOutput("result", "Record Inserted SuccessFully")
+
+	// Signal to the Flogo engine that the activity is completed
+	return true, nil
+}
